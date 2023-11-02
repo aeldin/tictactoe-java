361607b4402cffe544a31f73efa0446ef08024c6
Alfred <alfredbild@gmail.com>
2023-11-02 15:53:47 +0100

    Name change, basic gui, some methods

--- src/main/java/com/example/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/example/tictactoe/TicTacToeApplication.java
@@ -2,21 +2,18 @@ package com.example.tictactoe;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TicTacToeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-       Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeApplication.class.getResource("TicTacToe-view.fxml"));
+       Scene scene = new Scene(fxmlLoader.load(), 400, 400);
 
-        stage.setTitle("Hello!");
+        stage.setTitle("TicTacToe");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/tictactoe/TicTacToeController.java
+++ src/main/java/com/example/tictactoe/TicTacToeController.java
@@ -5,13 +5,27 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
-public class HelloController {
+import java.util.ArrayList;
 
-
-    public Button button1;
+public class TicTacToeController {
 
     public Label welcomeText;
-    public Label anotherLabel;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    ArrayList<Button> buttons;
+    public Label xCounter;
+    public Label yCounter;
+
+    private int playerTurn = 0;
+
+
 
     private Model model = new Model();
 
@@ -27,7 +41,8 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonAction() {
-        model.setMessage("Welcome to JavaFX Application!");
+
+        model.setMessage("Welcome to Tic-Tac-Toe!");
     }
 
 
